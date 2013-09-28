b5f163bd5e29986c1375b2c04dac1c8b24082db9
sam-at-github <sgpinkus@gmail.com>
2013-09-27 22:29:51 -0400

    Added init probably not working debian/.

---
+++ Makefile
@@ -0,0 +1,12 @@
+
+
+all:
+	echo all
+
+install:
+	cp firewall-iptables-deb /etc/init.d
+
+clean:
+	echo clean
+
+
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(firewall-iptables-deb):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="firewall-iptables-deb" command="/usr/bin/firewall-iptables-deb"
